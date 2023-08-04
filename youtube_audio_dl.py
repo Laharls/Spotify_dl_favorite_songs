8a5c1851fb99dac0c5fcc80d8eeae8ff2ab6c084
Anthony <anthony089@outlook.fr>
2023-08-04 14:23:36 +0200

    refactor: Refactoring code

---
+++ youtube_audio_dl.py
@@ -0,0 +1,39 @@
+import youtube_dl
+import googleapiclient.errors
+
+class YoutubeAudioDownloader:
+    def __init__(self, output_dir=None):
+        self.output_dir = output_dir
+
+    def get_youtube_video_id(youtube, song_title):
+        try:
+            search_response = youtube.search().list(
+                q=song_title,
+                part='id',
+                maxResults=1,
+                type='video'
+            ).execute()
+        except googleapiclient.errors.HttpError as e:
+            raise Exception(f"An error has occured while fetching Youtube video ID: {e}.")
+
+
+        if 'items' in search_response:
+            return search_response['items'][0]['id']['videoId']
+        else:
+            print("No items found in the API response.")
+            return None
+    
+    def download_audio(self, video_id):
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download(['https://www.youtube.com/watch?v=' + video_id])
+        except youtube_dl.utils.DownloadError as e:
+            raise Exception(f"An error has occured while downloading audio : {e}")
